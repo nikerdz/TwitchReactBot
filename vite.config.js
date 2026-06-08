c1799af1cb4637a62cd189b1ce66c8774e00f2a6
Anika Khan <121078391+nikerdz@users.noreply.github.com>
2026-06-08 19:28:17 +0000

    fix: build the front end and fix index.html paths

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   root: 'client',
+  base: './',
   plugins: [react()],
+  build: {
+    outDir: '../docs',
+    emptyOutDir: true
+  },
   server: {
     port: 5174,
     proxy: {
